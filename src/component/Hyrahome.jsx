8e52e6bfec634a6966bbd0dfea661b2b7aedb179
damola ifatifo <ifatifod4@gmail.com>
2024-02-15 17:09:14 +0100

    update

--- src/component/Hyrahome.jsx
+++ src/component/Hyrahome.jsx
@@ -4,16 +4,34 @@ const Hyrahome = () => {
   return (
     <div>
       <div className="flex-mother">
-         <div className="text">
-            <h1>Discover a world where education meets excitement</h1>
-            <p>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
-         </div>
-         <div className="img">
+        <div className="text">
+          <h1>Discover a world where education meets excitement</h1>
+          <p>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
+        </div>
+        <div className="img">
           <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1707921291/image_54_ymjfiz.png" alt="" />
-         </div>
+        </div>
       </div>
       <div className="why">
         <h1>Why Choose Us</h1>
+      </div>
+      <div className="box-flex">
+        <div className="box1">
+          <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708012712/image_235_hfqc4y.png" alt="" />
+          <p>Our app contains the fundamental teaching of Islam for Kids</p>
+        </div>
+        <div className="box2">
+          <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708012183/image_234_znite1.png" alt="" />
+          <p>We make it interesting by including the the Stories of the Prophets and his Companions </p>
+        </div>
+        <div className="box3">
+          <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708012183/image_220_fluc4n.png" alt="" />
+          <p>As we all know that Du’a is an essential part of our life as a Muslim. Du’a is also included in our app</p>
+        </div>
+        <div className="box4">
+          <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708013027/image_236_1_o4fugu.png" alt="" />
+          <p>Our app contains the 99 Names of Allah</p>
+        </div>
       </div>
     </div>
   )
