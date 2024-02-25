e7a928fabc3921e64f4fb756d4928a14e06c27c1
damola ifatifo <ifatifod4@gmail.com>
2024-02-25 12:32:06 +0100

    update

--- src/component/Ourteaching.jsx
+++ src/component/Ourteaching.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
-
+import "../assets/style/teach.css"
 const Ourteaching = () => {
   return (
-    <div>Ourteaching</div>
+    <div>
+      <div className="teachs">
+        <h1>Our Teachings</h1>
+        <div className="box-friends">
+          <div className="bxo1">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708781859/image_26_2_isq29b.png" alt="" />
+            <h5>Pillars of Islam: <br></br> The Pillars of Islam are essentials and must know for all Muslims</h5>
+          </div>
+          <div className="bxo2">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708781859/image_28_1_qoxt1z.png" alt="" />
+            <h5>Articles of Faith:
+              The Articles of faith are important part of Islamic Teachings</h5>
+          </div>
+          <div className="bxo3">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708781545/image_244_gd0q3k.png" alt="" />
+            <h5>Stories of the Prophet:
+              Allah sent the prophets to guide mankind, each prophet as his own followers. The stories of the Prophets teaches us some moral values to add to our daily lives</h5>
+          </div>
+          <div className="bxo4">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708780037/image_246_v2hsvu.png" alt="" />
+            <h5>Stories of the Companions:
+              The stories of the companions of Prophet Muhammad (SAW)  let us know the followers of the Prophet Muhammad (SAW) and those that ruled after the death of Prophet Muhammad(SAW)</h5>
+          </div>
+          <div className="bxo5">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708780036/image_221_mqrniv.png" alt="" />
+            <h5>Du’a:
+              Du’a is an essential part of the daily lives of every Muslims</h5>
+          </div>
+          <div className="bxo6">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708780036/image_249_f54mzf.png" alt="" />
+            <h5>99 Names of Allah:
+              It is good to call upon Allah using His names</h5>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
