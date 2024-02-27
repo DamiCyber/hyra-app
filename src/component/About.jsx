3868f61986ad792b6c1a249f6c4b052f77adcdb7
damola ifatifo <ifatifod4@gmail.com>
2024-02-27 13:22:33 +0100

    update

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import "../assets/style/about.css"
 const About = () => {
   return (
     <div>
+        <div className="about">
+           <div className="about-text">
+            <h1>Hyra Islamic Stories for Kids</h1><br />
+            <p>Hyra Islamic Stories for Kids is an engaging and interactive Educational app designed to introduce Islamic teachings to children.</p>
+           </div>
+           <div className="about-img">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1707921293/Property_1_image_53_c2jpjs.png" alt="" />
+           </div>
+        </div>
+        <div className="aboutwo ">
+           <div className="about-text ">
+            <p>It has been observed that some children lack proper fundamentals of Islamic teachings. Hyra is introduced to help kids know about the Islamic Teachings.</p>
+           </div>
+           <div className="about-img">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708780037/image_241_bqgpop.png" alt="" />
+           </div>
+        </div>
+        <div className="abouthree">
+           <div className="about-text">
+            <p>The target audience for this app is kids between the ages of 7-12</p>
+           </div>
+           <div className="about-img">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708780036/image_242_wvgvay.png" alt="" />
+           </div>
+        </div>
+        <div className="aboutfour">
+           <div className="about-text">
+            <p>The app combines Islamic teaching, Stories of the Prophet, Stories of the Companion, Du’a, quizzes and interactive activities to make learning about Islam enjoyable and fun for young mind</p>
+           </div>
+           <div className="about-img">
+            <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1708780036/image_243_ah5w6i.png" alt="" />
+           </div>
+        </div>
+         
 
     </div>
   )
