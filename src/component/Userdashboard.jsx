3be136ecc279c722fb8bd8853eb5b18270a42d15
damola ifatifo <ifatifod4@gmail.com>
2024-03-11 01:20:58 +0100

    update

--- src/component/Userdashboard.jsx
+++ src/component/Userdashboard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "../assets/style/dashboard.css"
+import { useNavigate } from 'react-router-dom';
 const Userdashboard = () => {
-  return (
-    <div>Userdashboard</div>
-  )
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 4000);
+    }, []);
+
+    if (loading) {
+        return <div>
+
+            <div class="loadingspinner">
+                <div id="square1"></div>
+                <div id="square2"></div>
+                <div id="square3"></div>
+                <div id="square4"></div>
+                <div id="square5"></div>
+            </div>
+
+        </div>
+    }
+    return (
+        <div>Userdashboard</div>
+    )
 }
 
 export default Userdashboard
