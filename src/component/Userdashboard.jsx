3031e76e3dedef9ad4bce319acc90897b2061664
damola ifatifo <ifatifod4@gmail.com>
2024-03-12 06:09:46 +0100

    update

--- src/component/Userdashboard.jsx
+++ src/component/Userdashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { getAuth } from "firebase/auth";
 import "../assets/style/dashboard.css"
 import { useNavigate } from 'react-router-dom';
 const Userdashboard = () => {
@@ -10,11 +11,21 @@ const Userdashboard = () => {
             setLoading(false);
         }, 6000);
     }, []);
+    const auth = getAuth();
+    const user = auth.currentUser;
 
+    if (user) {
+        console.log(user);
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        // ...
+    } else {
+        // No user is signed in.
+    }
     if (loading) {
         return <div>
             <div className="cover">
-            <div className="imag">
+                <div className="imag">
                     <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1707921289/logo_ozfrac.png" alt="" />
                 </div>
                 <div class="loadingspinner">
@@ -30,6 +41,7 @@ const Userdashboard = () => {
     }
     return (
         <div>Userdashboard</div>
+
         // https://the-trivia-api.com/v2/questions note api for the quiz
     )
 }
