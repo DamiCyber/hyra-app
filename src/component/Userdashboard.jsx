d0f25b6b68d2af0415aad986dab5e7fa8bcaab4f
damola ifatifo <ifatifod4@gmail.com>
2024-03-12 06:22:29 +0100

    update

--- src/component/Userdashboard.jsx
+++ src/component/Userdashboard.jsx
@@ -1,49 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { getAuth } from "firebase/auth";
-import "../assets/style/dashboard.css"
-import { useNavigate } from 'react-router-dom';
+import "../assets/style/dashboard.css";
+
 const Userdashboard = () => {
     const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+
     useEffect(() => {
         setLoading(true);
-        setTimeout(() => {
+        const auth = getAuth();
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setUser(user);
             setLoading(false);
-        }, 6000);
+        });
+        return () => unsubscribe();
     }, []);
-    const auth = getAuth();
-    const user = auth.currentUser;
 
-    if (user) {
-        console.log(user);
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        // ...
-    } else {
-        // No user is signed in.
-    }
     if (loading) {
-        return <div>
-            <div className="cover">
-                <div className="imag">
-                    <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1707921289/logo_ozfrac.png" alt="" />
-                </div>
-                <div class="loadingspinner">
-                    <div id="square1"></div>
-                    <div id="square2"></div>
-                    <div id="square3"></div>
-                    <div id="square4"></div>
-                    <div id="square5"></div>
+        return (
+            <div>
+                <div className="cover">
+                    <div className="imag">
+                        <img src="https://res.cloudinary.com/dhoqtwst9/image/upload/v1707921289/logo_ozfrac.png" alt="" />
+                    </div>
+                    <div className="loadingspinner">
+                        <div id="square1"></div>
+                        <div id="square2"></div>
+                        <div id="square3"></div>
+                        <div id="square4"></div>
+                        <div id="square5"></div>
+                    </div>
                 </div>
             </div>
-
-        </div>
+        );
+    } else {
+        if (user) {
+            return (
+                <div>
+                    <p>Welcome, {user.displayName}</p>
+                    <img src={user.photoURL} alt="" />
+                    {/* // https://the-trivia-api.com/v2/questions note api for the quiz */}
+                </div>
+            );
+        } else {
+            return <p>No user signed in.</p>;
+        }
     }
-    return (
-        <div>Userdashboard</div>
 
-        // https://the-trivia-api.com/v2/questions note api for the quiz
-    )
 }
 
-export default Userdashboard
+export default Userdashboard; 
